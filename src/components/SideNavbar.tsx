6e045bb14a880fc49a08a7eefeeb4f3e46af2403
hrmrakib <hrmrakibs@gmail.com>
2024-09-14 19:00:07 +0600

    design sidenavbar

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,9 +1,124 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+  IconContract,
+  IconHome, 
+  Icon
+} from "@tabler/icons-react";
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { cn } from "../lib/utils";
+import { BiSolidContact } from "react-icons/bi";
 
-const SideNavbar = () => {
+export function SidebarDemo() {
+  const links = [
+    {
+      label: "Home",
+      href: "#",
+      icon: <IconHome className='icon-style' />,
+    },
+    {
+      label: "About Me",
+      href: "#",
+      icon: <IconUserBolt className='icon-style' />,
+    },
+    {
+      label: "Service",
+      href: "#",
+      icon: <IconSettings className='icon-style' />,
+    },
+    {
+      label: "Skills",
+      href: "#",
+      icon: <IconArrowLeft className='icon-style' />,
+    },
+    {
+      label: "Experience",
+      href: "#",
+      icon: <IconArrowLeft className='icon-style' />,
+    },
+    {
+      label: "Portfolio",
+      href: "#",
+      icon: <IconArrowLeft className='icon-style' />,
+    },
+    {
+      label: "Blog",
+      href: "#",
+      icon: <IconArrowLeft className='icon-style' />,
+    },
+    {
+      label: "Contact",
+      href: "#",
+    //   icon: <BiSolidContact className='icon-style' />,
+      icon: <IconContract className='icon-style' />,
+    },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>SideNavbar</div>
-  )
+    <div className={cn("container-style")}>
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className='sidebar-body-style'>
+          <div className='sidebar-content'>
+            {open ? <Logo /> : <LogoIcon />}
+            <div className='links-style'>
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+
+          <SidebarLink
+            link={{
+              label: "Manu Arora",
+              href: "#",
+              icon: (
+                <Image
+                  src=''
+                  className='avatar-style'
+                  width={50}
+                  height={50}
+                  alt='Avatar'
+                />
+              ),
+            }}
+          />
+        </SidebarBody>
+      </Sidebar>
+      <Dashboard />
+    </div>
+  );
 }
 
-export default SideNavbar
+export const Logo = () => (
+  <Link href='#' className='logo-style'>
+    <div className='logo-box' />
+    <motion.span
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className='logo-text'
+    >
+      Acet Labs
+    </motion.span>
+  </Link>
+);
+
+export const LogoIcon = () => (
+  <Link href='#' className='logo-style'>
+    <div className='logo-box' />
+  </Link>
+);
+
+const Dashboard = () => (
+  <div className='overflow-y-scroll'>
+    
+  </div>
+);
