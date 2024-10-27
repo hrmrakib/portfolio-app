944311633d54a61ba6e038907f28cdd55e9fac4e
hrmrakib <hrmrakibs@gmail.com>
2024-10-27 10:05:08 +0600

    update: my resume with link

--- src/lib/handleDownload.ts
+++ src/lib/handleDownload.ts
@@ -1,15 +1,14 @@
 const handleDownload = () => {
-    const pdfUrl = "Rakibul_Alam_Frontend_Engineer.pdf";
-  
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.setAttribute("download", "Rakibul_Alam_Frontend_Engineer"); 
-  
-    document.body.appendChild(link);
-    link.click();
-  
-    document.body.removeChild(link);
-  };
-  
-  export default handleDownload;
-  
+  const pdfUrl = "Rakibul_Alam_Frontend_Developer.pdf";
+
+  const link = document.createElement("a");
+  link.href = pdfUrl;
+  link.setAttribute("download", "Rakibul_Alam_Frontend_Developer");
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+};
+
+export default handleDownload;
