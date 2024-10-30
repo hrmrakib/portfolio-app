77cf20ce993e62a743a84824a79a0922fd0c1361
hrmrakib <hrmrakibs@gmail.com>
2024-10-30 22:02:00 +0600

    match color combination side navbar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -181,7 +181,7 @@ export const SidebarLink = ({
           display: animate ? (open ? "inline-block" : "none") : "inline-block",
           opacity: animate ? (open ? 1 : 0) : 1,
         }}
-        className='text-neutral-700 dark:text-neutral-200 text-sm group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0'
+        className='text-neutral-200 text-sm group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0'
       >
         {link.label}
       </motion.span>
