cf5c628bb3a4e0565d68ae999d2dec97018d1b8d
hrmrakib <hrmrakibs@gmail.com>
2024-12-15 09:53:42 +0600

    update resume

--- src/lib/handleDownload.ts
+++ src/lib/handleDownload.ts
@@ -1,10 +1,10 @@
 const handleDownload = () => {
-  const pdfUrl = "Rakibul_Alam_Frontend_Developer.pdf";
+  const pdfUrl = "Rakibul_Alam_Web_Developer.pdf";
 
   const link = document.createElement("a");
 
   link.href = pdfUrl;
-  link.setAttribute("download", "Rakibul_Alam_Frontend_Developer");
+  link.setAttribute("download", "Rakibul_Alam_Web_Developer");
 
   document.body.appendChild(link);
   link.click();
