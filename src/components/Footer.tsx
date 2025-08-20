b594ede849298e407f5c2d12c3ec391b38814ec1
hrmrakib <hrmrakibs@gmail.com>
2025-08-20 16:02:57 +0600

    update: dynamic current year in footer sec

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,17 @@
-import FloatingDockDemo from "@/components/SocialLink"
+import FloatingDockDemo from "@/components/SocialLink";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className='px-4 pt-2 bg-[#02050A] text-[#a2a2a2]'>
       <div className='border-t border-t-gray-700 w-[100%] mx-auto'></div>
       <div className='pt-12 px-2 lg:px-5 flex items-center justify-between'>
-        <p className='text-sm'>&copy; Rakibul 2024 | All Rights Reserved</p>
-        
+        <p className='text-sm'>
+          &copy; Rakibul {currentYear} | All Rights Reserved
+        </p>
+
         <div>
-            <FloatingDockDemo />
+          <FloatingDockDemo />
         </div>
       </div>
     </footer>
